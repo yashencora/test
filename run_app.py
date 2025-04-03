cc84e4f7eabc2c211554710b09e933abaf3a941b
yashencora <yash.nanaware@encora.com>
2025-04-03 06:57:48 +0000

    commit_check4

--- run_app.py
+++ run_app.py
@@ -1,1 +1,1 @@
-print("hi,Hello,hey there, how are you? fine. I am here yeah!!! Hi There howare fine.Hey hey")
+print("hi,Hello,hey there, how are you? fine. I am here yeah!!! Hi There howare fine.Hey hey se")
--- testcode.java
+++ testcode.java
@@ -1,6 +1,6 @@
 public class FibonacciSeries {
     public static void main(String[] args) {
-        int n = 10; // Number of terms in the Fibonacci seri
+        int n = 10; // Number of terms in the Fibonacci series
         fibonacci(n);
     }
 
