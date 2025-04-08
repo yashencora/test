fa449d098eb224ec87cf259cd81add4b70d8c381
yashencora <yash.nanaware@encora.com>
2025-04-08 10:32:14 +0000

    commit_25

--- run_test_app.py
+++ run_test_app.py
@@ -1,7 +1,9 @@
 def is_prime(n):
     if n <= 1:
+        print("FALSE")
         return False
     if n <= 3:
+        print("TRUE")
         return True
     if n % 2 == 0 or n % 3 == 0:
         return False
@@ -15,7 +17,8 @@ def is_prime(n):
 
 # Example usage
 num = 29
+print("REACHED HERE")
 if is_prime(num):
-    print(f"{num} is a prime number.")
+    print(f"{num} is a prime number.PRIME")
 else:
-    print(f"{num} is not a prime number. and do the execution, for running,testing function here.  ")
+    print(f"{num} is not a prime number. and do the execution, for running,testing function here.NON-PRIME")
