b595ac23c278fb050db534a1bd6a265ea51f9a23
yashencora <yash.nanaware@encora.com>
2025-04-09 07:12:42 +0000

    commit_1_09/04

--- run_test_app.py
+++ run_test_app.py
@@ -19,6 +19,6 @@ def is_prime(n):
 num = 29
 print("REACHED HERE")
 if is_prime(num):
-    print(f"{num} is a prime number.PRIME")
+    print(f"{num} is a prime number.")
 else:
-    print(f"{num} is not a prime number. and do the execution, for running,testing function here.NON-PRIME")
+    print(f"{num} is not a prime number. and do the execution, for running,testing function here.")
