6cd6cd6280254e340af0adf5dbd0935dff48a85f
yashencora <yash.nanaware@encora.com>
2025-04-09 07:51:22 +0000

    commit_3_09/04

--- testcode.java
+++ testcode.java
@@ -7,7 +7,7 @@ public class FibonacciSeries {
     public static void fibonacci(int n) {
         int first = 0, second = 1;
 
-        System.out.println("Fibonacci Series up to " + n + " terms:");
+        System.out.println("Fibonacci Series up to " + n + " terms: ");
         for (int i = 0; i < n; i++) {
             System.out.print(first + " ");
             int next = first + second;
